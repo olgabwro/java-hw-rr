1439d334aa7b9a937985febd8760546e0b36ce5b
olgabwro <olgabwro@gmail.com>
2025-09-01 15:13:59 +0200

    Home work04 (#6)
    * create:HW04_01
    
    * create:HW04_02

---
+++ src/homeWork04/HW04_02.java
@@ -0,0 +1,45 @@
+package homeWork04;
+
+import java.util.Arrays;
+
+public class HW04_02 {
+    public static void main(String[] args) {
+        /**
+         * Задача №1. Дан массив:
+         * int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+         * необходимо вывести все нечетные числа из массива.
+         */
+        System.out.println("Task1");
+
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.println(array[i]);
+            }
+        }
+
+        /* Задача №2. Дан массив:
+         * int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+         * необходимо вывести все значения массива больше 5.
+         */
+        System.out.println("Task2");
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 5) {
+                System.out.println(array[i]);
+            }
+        }
+
+        /* Задача №3.Дан массив:
+         * int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+         * необходимо увеличить все значения массива на 15.
+         */
+
+        System.out.println("Task3");
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= 15;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
